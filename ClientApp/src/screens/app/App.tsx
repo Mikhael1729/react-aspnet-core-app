2c97cdcf6ba422f9dc5415043fca6afb61d029ce
Mikhael Santos Fernández <Mikhael1729@gmail.com>
2020-06-26 02:39:23 -0400

    Delete unused elements

--- ClientApp/src/screens/app/App.tsx
+++ ClientApp/src/screens/app/App.tsx
@@ -1,11 +1,8 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Layout } from '../../layout/Layout';
-import { Home } from '../home/Home';
 import { DetalleSituacionReport } from '../DetalleSituacionReport/DetalleSituacionReport';
-import { Counter } from '../counter/Counter';
 import { SituacionActual } from "../report-charts/report-charts"
-import { AddTreatment } from "../add-treatment/add-treatment";
 
 export default class App extends Component {
   static displayName = App.name;
@@ -14,7 +11,6 @@ export default class App extends Component {
     return (
       <Layout>
         <Route exact path="/" component={DetalleSituacionReport} />
-        <Route path="/counter" component={Counter} />
         <Route path="/situacion-actual" component={SituacionActual} />
       </Layout>
     );
